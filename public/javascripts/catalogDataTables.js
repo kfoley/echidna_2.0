e2546a7c152051b56781cc9f8fcab5f96140c8d7
kfoley <kfoley73@gmail.com>
2011-03-29 12:02:33 -0700

    added Datatables files to track

---
+++ public/javascripts/catalogDataTables.js
@@ -0,0 +1,12 @@
+/*
+* This is used to call the DataTables jquery library and render
+* the documents found via search in a DataTable format
+*
+*/
+
+var oTable;
+$(document).ready(function() {
+     /* Initialise datatables*/ 
+     oTable = $('.dataTable').dataTable(); 
+});
+
